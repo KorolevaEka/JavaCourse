4b71ddc79085c33741c097150dd6e8069046a9fb
ekaterinakoroleva <ekoroleva@taxdome.com>
2025-05-25 10:54:58 +0200

    add ui test for selenium

---
+++ src/test/java/main/HomePageTest.java
@@ -0,0 +1,46 @@
+package main;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class HomePageTest {
+    WebDriver driver;
+    private static String BASE_URL = "https://bonigarcia.dev/selenium-webdriver-java/";
+
+    @BeforeEach
+    void setup() {
+        driver = new ChromeDriver();
+        driver.get(BASE_URL);
+        driver.manage().window().maximize();
+    }
+
+    @AfterEach
+    void tearDown() {
+        driver.quit();
+    }
+
+    @Test
+    void openWebFormTest() {
+        String webFormUrl = "web-form.html";
+        driver.findElement(By.linkText("Web form")).click();
+        String currentUrl = driver.getCurrentUrl();
+        WebElement title = driver.findElement(By.className("display-6"));
+
+        assertEquals(BASE_URL + webFormUrl, currentUrl);
+        assertEquals("Web form", title.getText());
+    }
+
+    @Test
+    void openHomePageTest() {
+        String actualTitle = driver.getTitle();
+
+        assertEquals("Hands-On Selenium WebDriver with Java", actualTitle);
+    }
+}
